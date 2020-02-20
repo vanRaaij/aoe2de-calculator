86e18b6658ce50a04a9fb0ce38d24c543e81d5a4
Simon van Raaij <simon.van.raaij@topicus.nl>
2020-02-20 10:41:25 +0100

    Removed uglify

--- build.sh
+++ build.sh
@@ -2,13 +2,9 @@
 
 set -e
 
-js="elm.js"
-min="target/elm.min.js"
+js="build/elm.js"
 
 elm make src/Main.elm --optimize --output=$js $@
 
-uglify-js $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglify-js --mangle --output=$min
-
 echo "Compiled size:$(cat $js | wc -c) bytes  ($js)"
-echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
-echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
+echo "Gzipped size: $(cat $js | gzip -c | wc -c) bytes"
