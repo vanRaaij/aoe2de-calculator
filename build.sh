0e16257b349664b8c81dc8ddccd475f866feff5d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-20 09:42:38 +0000

    Deploy vanRaaij/aoe2de-calculator to github.com/vanRaaij/aoe2de-calculator.git:gh-pages

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
-elm make src/Main.elm --optimize --output=elm.js
-uglifyjs elm.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=target/elm.min.js
+#!/bin/sh
+
+set -e
+
+js="build/elm.js"
+
+elm make src/Main.elm --optimize --output=$js $@
+
+echo "Compiled size:$(cat $js | wc -c) bytes  ($js)"
+echo "Gzipped size: $(cat $js | gzip -c | wc -c) bytes"
