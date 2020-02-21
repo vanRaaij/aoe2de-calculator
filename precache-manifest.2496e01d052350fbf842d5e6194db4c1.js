8572ae39c23129fa8a9ada621871764677a5c07d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-21 12:43:20 +0000

    Deploy vanRaaij/aoe2de-calculator to github.com/vanRaaij/aoe2de-calculator.git:gh-pages

---
+++ precache-manifest.2496e01d052350fbf842d5e6194db4c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "3b3a3bde20ec70f9697c64693315e272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "916d86099c465379cfbb",
+    "url": "/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "916d86099c465379cfbb",
+    "url": "/static/js/main.916d8609.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "/static/js/vendors~main.604c6712.chunk.js"
+  }
+]);
